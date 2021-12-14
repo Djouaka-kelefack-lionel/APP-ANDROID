be2eca2f29b27dced65712c9e34c943e3b9561b1
djouaka <djouakalionel4@gmail.com>
2021-12-14 15:27:17 +0100

    Modelizazione delle diversi classi per la geszione dei fagmenti e diversi layout

--- app/src/main/java/com/lionel/appandroid/HomeFragment.java
+++ app/src/main/java/com/lionel/appandroid/HomeFragment.java
@@ -11,6 +11,8 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.facebook.shimmer.ShimmerFrameLayout;
+
 import java.util.ArrayList;
 
 import retrofit2.Call;
@@ -24,6 +26,8 @@ public class HomeFragment extends Fragment {
     Adapter adapter;
     String country = "in";
     private RecyclerView recyclerViewofhome;
+    MainActivity mainActivity;
+    ShimmerFrameLayout shimmerFrameLayout;
 
     @Nullable
     @Override
@@ -31,6 +35,8 @@ public class HomeFragment extends Fragment {
         View v = inflater.inflate(R.layout.homefragment, null);
 
         recyclerViewofhome = v.findViewById(R.id.recyclerviewofhome);
+        shimmerFrameLayout = v.findViewById(R.id.shimmer);
+        shimmerFrameLayout.startShimmer();
         modelClassArrayList = new ArrayList<>();
         recyclerViewofhome.setLayoutManager(new LinearLayoutManager(getContext()));
         adapter = new Adapter(getContext(),modelClassArrayList);
@@ -47,7 +53,11 @@ public class HomeFragment extends Fragment {
         ApiUtilities.getApiInterface().getNews(country, 100,api).enqueue(new Callback<MainNews>() {
             @Override
             public void onResponse(Call<MainNews> call, Response<MainNews> response) {
-                if (response.isSuccessful()){
+                if (response.isSuccessful())
+                {
+                    shimmerFrameLayout.stopShimmer();
+                    shimmerFrameLayout.setVisibility(View.GONE);
+                    recyclerViewofhome.setVisibility(View.VISIBLE);
                     modelClassArrayList.addAll(response.body().getArticles());
                     adapter.notifyDataSetChanged();
                 }
