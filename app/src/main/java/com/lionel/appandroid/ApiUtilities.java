a5fdace082918a639980fac5cb8cc13b7fb1b4ff
djouaka <djouakalionel4@gmail.com>
2021-12-14 10:19:39 +0100

    Creazione dell'interfacia  per la gestione dell'APi/coretto

--- app/src/main/java/com/lionel/appandroid/ApiUtilities.java
+++ app/src/main/java/com/lionel/appandroid/ApiUtilities.java
@@ -1,17 +1,19 @@
 package com.lionel.appandroid;
 
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiUtilities {
 
     private static Retrofit retrofit = null;
 
-    public static getApiInterface(){
+    public static ApiInterface getApiInterface(){
 
         if (retrofit == null){
 
-            retrofit = new Retrofit.Builder().baseUrl(ApiInterfice.BASE_URL).addConverterFactory()
+            retrofit = new Retrofit.Builder().baseUrl(ApiInterface.BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
         }
+        return retrofit.create(ApiInterface.class);
     }
 
 }
